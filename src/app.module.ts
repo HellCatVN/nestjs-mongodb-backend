ccfa2229ab87cca49e1bdeb7d941c3d3f28c20d9
HellCatVN <hellcatvn@gmail.com>
2021-02-05 01:12:20 +0700

    Add configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +8,17 @@ import { CatsModule } from './modules/cat/cats.module';
 @Module({
   imports: [
     CatsModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/Elves'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
